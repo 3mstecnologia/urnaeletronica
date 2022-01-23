c13ff68788083d5962d9c80ae7d52e3fc4f43af7
3mstecnologia <matheus@>
2022-01-23 20:45:49 -0300

    Alterado Estrutura de Arquivos

---
+++ etapas.js
@@ -0,0 +1,46 @@
+let etapas = [
+  {
+    titulo: "VEREADOR",
+    numeros: 5,
+    candidatos: [
+      {
+        numero: "38111",
+        nome: "Victor Bridi",
+        partido: "ABC",
+        fotos: [{ url: "38111.jpg", legenda: "Vereador" }],
+      },
+      {
+        numero: "77222",
+        nome: "Shirley Patricia",
+        partido: "Partido das Indias Partidas",
+        fotos: [{ url: "77222.jpg", legenda: "Mãe Solteira Procura" }],
+      },
+    ],
+  },
+  {
+    titulo: "PREFEITO",
+    numeros: 2,
+    candidatos: [
+      {
+        numero: "88",
+        nome: "Matheus",
+        partido: "PM",
+        vice: "Cic",
+        fotos: [
+          { url: "99.jpg", legenda: "Prefeito" },
+          { url: "99_2.jpg", legenda: "Vice-Prefeito", small: true },
+        ],
+      },
+      {
+        numero: "84",
+        nome: "Michele",
+        partido: "RAINHA DE TUDO",
+        vice: "Zul",
+        fotos: [
+          { url: "84.jpg", legenda: "Prefeito" },
+          { url: "84_2.jpg", legenda: "Vice-Prefeito", small: true },
+        ],
+      },
+    ],
+  },
+];
