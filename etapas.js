779130eb4dadc0c5cb50a6f5027d5e71de38edca
3mstecnologia <matheus@>
2022-01-23 20:47:31 -0300

    Etapas

--- etapas.js
+++ etapas.js
@@ -1,16 +1,16 @@
 let etapas = [
   {
-    titulo: "VEREADOR",
+    titulo: "PIOR FOTO",
     numeros: 5,
     candidatos: [
       {
-        numero: "38111",
+        numero: "11111",
         nome: "Victor Bridi",
-        partido: "ABC",
+        partido: "Viciado em Celular",
         fotos: [{ url: "38111.jpg", legenda: "Vereador" }],
       },
       {
-        numero: "77222",
+        numero: "22222",
         nome: "Shirley Patricia",
         partido: "Partido das Indias Partidas",
         fotos: [{ url: "77222.jpg", legenda: "Mãe Solteira Procura" }],
@@ -18,24 +18,24 @@ let etapas = [
     ],
   },
   {
-    titulo: "PREFEITO",
+    titulo: "REI DO MUNDO",
     numeros: 2,
     candidatos: [
       {
         numero: "88",
-        nome: "Matheus",
+        nome: "Matheus Schmitt",
         partido: "PM",
-        vice: "Cic",
+        vice: "Michele Temchena",
         fotos: [
           { url: "99.jpg", legenda: "Prefeito" },
           { url: "99_2.jpg", legenda: "Vice-Prefeito", small: true },
         ],
       },
       {
-        numero: "84",
+        numero: "11",
         nome: "Michele",
         partido: "RAINHA DE TUDO",
-        vice: "Zul",
+        vice: "Eu Mesmo",
         fotos: [
           { url: "84.jpg", legenda: "Prefeito" },
           { url: "84_2.jpg", legenda: "Vice-Prefeito", small: true },
